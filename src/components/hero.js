79bb9de8e8a192f2333075972e6612ad2676e7bb
Icejewel0319 <icejewel0319@gmail.com>
2020-03-23 14:45:55 -0700

    sliding

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,19 +1,66 @@
 import React from 'react'
 import Img from 'gatsby-image'
-
+import { Carousel } from 'react-bootstrap'
 import styles from './hero.module.css'
 
 export default ({ data }) => (
   <div className={styles.hero}>
+  <Carousel>
+  <Carousel.Item>
     <Img
       className={styles.heroImage}
       alt={data.name}
       fluid={data.heroImage.fluid}
     />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
-    </div>
-  </div>
+    <Carousel.Caption>
+      <div className={styles.heroDetails}>
+        <h3 className={styles.heroHeadline}>{data.name}</h3>
+        <p className={styles.heroTitle}>{data.title}</p>
+        <p>{data.shortBio.shortBio}</p>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Img
+        className={styles.heroImage}
+        alt={data.name}
+        fluid={data.heroImage.fluid}
+      />
+    <Carousel.Caption>
+      <div className={styles.heroDetails}>
+        <h3 className={styles.heroHeadline}>{data.name}</h3>
+        <p className={styles.heroTitle}>{data.title}</p>
+        <p>{data.shortBio.shortBio}</p>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Img
+      className={styles.heroImage}
+      alt={data.name}
+      fluid={data.heroImage.fluid}
+    />
+    <Carousel.Caption>
+      <div className={styles.heroDetails}>
+        <h3 className={styles.heroHeadline}>{data.name}</h3>
+        <p className={styles.heroTitle}>{data.title}</p>
+        <p>{data.shortBio.shortBio}</p>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
+
+  // <div className={styles.hero}>
+  //   <Img
+  //     className={styles.heroImage}
+  //     alt={data.name}
+  //     fluid={data.heroImage.fluid}
+  //   />
+  //   <div className={styles.heroDetails}>
+  //     <h3 className={styles.heroHeadline}>{data.name}</h3>
+  //     <p className={styles.heroTitle}>{data.title}</p>
+  //     <p>{data.shortBio.shortBio}</p>
+  //   </div>
+  // </div>
 )
